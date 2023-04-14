d3c101bbc8b2041b10b82384260068581d5a4de1
Lucas Averty <lu.averty@hotmail.fr>
2023-04-14 11:08:25 +0200

    Done : Display One Flat - TODO : Make a grid

--- src/components/flat.js
+++ src/components/flat.js
@@ -2,18 +2,27 @@ import React from 'react';
 import "./flat.css"
 
 const Flat = ({ flat }) => {
+  const style = {
+    backgroundImage: `url('${flat.imageUrl}')`,
+    backgroundSize: 'cover', // Add this line to cover the div with the image
+    backgroundPosition: 'center', // Add this line to position the image at the center
+    // height: '400px', // Add this line to set the height of the div
+  };
   return (
-    <div>
-      <h2>{flat.name}</h2>
-      <img src={flat.imageUrl} alt={flat.name} />
-      <p>Price: {flat.price} {flat.priceCurrency}</p>
-      <p>Latitude: {flat.lat}</p>
-      <p>Longitude: {flat.lng}</p>
+    <div className="flat">
+      <div className="flat-picture" style={style}></div>
+      <div className ="flat-title">
+        <h2>{flat.name}</h2>
+        <p>Price: {flat.price} {flat.priceCurrency}</p>
+        <p>Latitude: {flat.lat}</p>
+        <p>Longitude: {flat.lng}</p>
+      </div>
     </div>
   );
 }
 
 export default Flat;
+
 
 // class Flat  extends React.Component {
 //   render() {
@@ -28,7 +37,7 @@ export default Flat;
 //     return
 //     <div className="flat">
 //       <div className="flat-picture" style={style}></div>
-//       <div className="flat-title ">
+//       <div className="flat-title">
 //         {title}
 //       </div>
 //     </div>
